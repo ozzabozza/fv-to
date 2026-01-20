8dad9a986309f6e8356b6b08b84a6c4dbe541098
Oscar Untied <oscaruntied@gmail.com>
2026-01-20 22:45:55 +0100

    Added joke list item to favorites page

--- src/app/features/favorites/favorites.ts
+++ src/app/features/favorites/favorites.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { JokeListItemComponent } from '../../shared/components/joke-list-item/joke-list-item';
+import { FavoritesService } from '../../shared/services/state/favorites';
 
 @Component({
   selector: 'app-favorites',
-  imports: [],
+  imports: [JokeListItemComponent],
   templateUrl: './favorites.html',
   styleUrl: './favorites.scss',
 })
-export class FavoritesComponent { }
+export class FavoritesComponent {
+  favoritesService = inject(FavoritesService);
+}
