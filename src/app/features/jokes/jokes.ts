ace99e88e42be9b41191698b03d8727d67b3be81
Oscar Untied <oscaruntied@gmail.com>
2026-01-20 21:21:50 +0100

    Added jokes item to jokes page

--- src/app/features/jokes/jokes.ts
+++ src/app/features/jokes/jokes.ts
@@ -1,13 +1,16 @@
 import { Component, inject } from '@angular/core';
 
+import { JokeListItemComponent } from '../../shared/components/joke-list-item/joke-list-item';
+import { FavoritesService } from '../../shared/services/state/favorites';
 import { JokesService } from '../../shared/services/state/jokes';
 
 @Component({
   selector: 'app-jokes',
-  imports: [],
+  imports: [JokeListItemComponent],
   templateUrl: './jokes.html',
   styleUrl: './jokes.scss',
 })
 export class JokesComponent {
   jokesService = inject(JokesService);
+  favoritesService = inject(FavoritesService);
 }
